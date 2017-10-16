456742c68177c11c6aa58242ba8d99f3813f1d2d
Lulu Liu <luluonino@gmail.com>
2017-10-15 18:11:53 -0700

    update

--- percolation/src/main/java/Percolation.java
+++ percolation/src/main/java/Percolation.java
@@ -1,5 +1,3 @@
-// default package
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
@@ -7,27 +5,30 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  * The top and bottom rows are open and connected as the two ends.
  */
 public class Percolation {
-  private int n;
-  private int[][] status;
+  private final int n;
+  private final boolean[][] status;
   private int numOpen;
-  private WeightedQuickUnionUF uf; // 0-indexed union-find with size n+2
+  private final WeightedQuickUnionUF uf; // 0-indexed union-find with size n+2
+  private final WeightedQuickUnionUF ufFullness;
 
-  public Percolation(int n) throws IllegalArgumentException {
+  public Percolation(int n) {
     if (n <= 0) {
       throw new IllegalArgumentException();
     }
 
     this.n = n;
-    this.status = new int[n+2][n+2];
-    for (int i=0; i<n+2; i++) {
-      this.status[0][i] = 1; // top row
-      this.status[n+1][i] = 1; // bottom row
+    this.status = new boolean[n+2][n+2];
+    for (int i = 0; i < n+2; i++) {
+      this.status[0][i] = true; // top row
+      this.status[n+1][i] = true; // bottom row
     }
     this.numOpen = 0;
     this.uf = new WeightedQuickUnionUF((n+2)*(n+2));
-    for (int i=1; i< n+2; i++) {
-      this.uf.union(0, i); // union first row 0 - n+1
-      this.uf.union((n+2)*(n+1), (n+2)*(n+1)+i);
+    this.ufFullness = new WeightedQuickUnionUF((n+2)*(n+2));
+    for (int i = 1; i < n+2; i++) {
+      this.uf.union(ufIndex(0, 0), ufIndex(0, i));
+      this.uf.union(ufIndex(n+1, 0), ufIndex(n+1, i));
+      this.ufFullness.union(ufIndex(0, 0), ufIndex(0, i));
     }
   }
 
@@ -35,32 +36,57 @@ public class Percolation {
     return row*(n+2)+col;
   }
 
-  public void open(int row, int col) throws IllegalArgumentException { // row, col are 1-indexed
-    if (row <1 || row > this.n || col < 1 || col > this.n) {
+  public void open(int row, int col) { // row, col are 1-indexed
+    if (row < 1 || row > this.n || col < 1 || col > this.n) {
       throw new IllegalArgumentException();
     }
-    this.status[row][col] = 1;
+
+    if (this.isOpen(row, col)) {
+      return;
+    }
+
+    this.status[row][col] = true;
     this.numOpen += 1;
-    if (isOpen(row-1, col)) {
+
+    if (row == 1) {
+      this.uf.union(ufIndex(0, col), ufIndex(row, col));
+      this.ufFullness.union(ufIndex(0, col), ufIndex(row, col));
+    }
+    else if (isOpen(row-1, col)) {
       this.uf.union(ufIndex(row-1, col), ufIndex(row, col));
+      this.ufFullness.union(ufIndex(row-1, col), ufIndex(row, col));
     }
-    if (isOpen(row, col-1)) {
-      this.uf.union(ufIndex(row, col-1), ufIndex(row, col));
+
+    if (row == n) {
+      this.uf.union(ufIndex(n+1, col), ufIndex(row, col));
     }
-    if (isOpen(row+1, col)) {
+    else if (isOpen(row+1, col)) {
       this.uf.union(ufIndex(row+1, col), ufIndex(row, col));
+      this.ufFullness.union(ufIndex(row+1, col), ufIndex(row, col));
     }
-    if (isOpen(row, col+1)) {
+
+    if (col > 1 && isOpen(row, col-1)) {
+      this.uf.union(ufIndex(row, col-1), ufIndex(row, col));
+      this.ufFullness.union(ufIndex(row, col-1), ufIndex(row, col));
+    }
+    if (col < n && isOpen(row, col+1)) {
       this.uf.union(ufIndex(row, col+1), ufIndex(row, col));
+      this.ufFullness.union(ufIndex(row, col+1), ufIndex(row, col));
     }
   }
 
   public boolean isOpen(int row, int col) {  // is site (row, col) open?
-    return this.status[row][col] == 1;
+    if (row < 1 || row > this.n || col < 1 || col > this.n) {
+      throw new IllegalArgumentException();
+    }
+    return this.status[row][col];
   }
 
   public boolean isFull(int row, int col) { // is site (row, col) full?
-    return this.status[row][col] == 0;
+    if (row < 1 || row > this.n || col < 1 || col > this.n) {
+      throw new IllegalArgumentException();
+    }
+    return this.ufFullness.connected(ufIndex(0, 0), ufIndex(row, col));
   }
 
   public int numberOfOpenSites() { // number of open sites
---
+++ percolation/src/main/java/PercolationStats.java
@@ -0,0 +1,80 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+  private static final double CONFIDENCE_95 = 1.96;
+  private final int n;
+  private final int trials;
+  private final double[] sample;
+  private double mean;
+  private double stddev;
+  private double confidenceLo;
+  private double confidenceHi;
+
+  public PercolationStats(int n, int trials) {
+    if (n <= 0 || trials <= 0) {
+      throw new IllegalArgumentException();
+    }
+    this.n = n;
+    this.trials = trials;
+    this.sample = new double[trials];
+    this.doExperiment();
+  }
+
+  private void doExperiment() {
+    for (int i = 0; i < this.trials; i++) {
+      Percolation percolation = new Percolation(this.n);
+      double threshold = 0.;
+      while (true) {
+        int row = StdRandom.uniform(this.n) + 1;
+        int col = StdRandom.uniform(this.n) + 1;
+        if (percolation.isOpen(row, col)) { // don't reopen open ones
+          continue;
+        }
+        percolation.open(row, col);
+        threshold += 1.;
+        if (percolation.percolates()) {
+          this.sample[i] = threshold / (this.n*this.n);
+          break;
+        }
+      }
+    }
+    this.mean = StdStats.mean(this.sample);
+    this.stddev = StdStats.stddev(this.sample);
+    this.confidenceLo = this.mean - this.stddev * CONFIDENCE_95 / Math.sqrt(this.trials);
+    this.confidenceHi = this.mean + this.stddev * CONFIDENCE_95 / Math.sqrt(this.trials);
+  }
+
+  public double mean() {
+    return this.mean;
+  }
+
+  public double stddev() {
+    return this.stddev;
+  }
+
+  public double confidenceLo() {
+    return this.confidenceLo;
+  }
+
+  public double confidenceHi() {
+    return this.confidenceHi;
+  }
+
+  public static void main(String[] args) {
+    int n = 0;
+    int trials = 0;
+    if (args.length == 2) {
+      n = Integer.parseInt(args[0]);
+      trials = Integer.parseInt(args[1]);
+    }
+
+    PercolationStats percolationStats = new PercolationStats(n, trials);
+    System.out.println("mean                    = " + percolationStats.mean());
+    System.out.println("stddev                  = " + percolationStats.stddev());
+    System.out.println("95% confidence interval = ["
+      + percolationStats.confidenceLo() + ", "
+      + percolationStats.confidenceHi() + "]");
+  }
+
+}
